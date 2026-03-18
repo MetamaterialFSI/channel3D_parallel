b1f19c610068f9bcffdf5e0b8561a1dee5d7dffc
diederikb <beckersdiederik@gmail.com>
2026-03-17 19:45:55 -0700

    Remove ching-te's test script

--- test_script.sh
+++
@@ -1,8 +0,0 @@
-module load fftw
-module load intel-oneapi-compilers
-module load openmpi
-cmake -B build -DCMAKE_BUILD_TYPE=Debug
-cmake --build build 
-cd ./examples/
-#rm -rf ./channel_output_ib_planar_wall/
-sbatch jobscript_ib_planar_wall.slurm
