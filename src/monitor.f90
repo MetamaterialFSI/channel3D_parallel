934734cce24b9efa6072d99132ad9d6e3465927d
diederikb <beckersdiederik@gmail.com>
2026-03-19 12:59:43 -0700

    Revert to original IBPM

--- src/monitor.f90
+++ src/monitor.f90
@@ -225,7 +225,7 @@ Contains
     Real(Int64), Intent(Out) :: max_slip
 
     Call regT(Eu, U, V, W)
-    aux_surface_vector = Eu - E1nH_exterior * dudn_jump - ub
+    aux_surface_vector = Eu - ub
     max_slip = Maxval( Abs(aux_surface_vector) )
 
   end subroutine check_slip
